95e8f49d2eee83bdc06d947408d88f92389e9c6b
WuIFan <denny91002@gmail.com>
2016-12-12 20:33:57 +0800

    rough tail packing

---
+++ mycdc.py
@@ -0,0 +1,153 @@
+#-*-coding:utf-8 -*-
+
+clustersize=1024*4
+window=48
+base=101
+"""
+avsize=1024*4
+minsize=1024
+maxsize=1024*8
+"""
+avsize=1024*4
+minsize=1024
+maxsize=1024*32
+#mod=119870
+mod=1000000000
+basewindow=1
+for a in range(0,window):	#用來計算rabin的第一個值
+	basewindow=(basewindow*base)%mod
+
+def puretxt(a,b):#純文字檔(去空格空白)，可以不用做
+	#print (a.read())
+	for line in a:
+		temp=line.strip()
+		temp1=temp.split(' \t')
+		temp="".join(temp1)
+		#print temp;
+		b.write(temp)
+
+def rb(a):
+	asc=[]
+	for line in a:		
+		for c in line:
+			temp=ord(c)
+			asc.append(temp)
+	#print(len(asc)-window)
+	hashv=[]
+	cutpoint=[]
+	sizeofchunk=[]
+	tempsize=9
+	check=0
+	for i in range(0,(len(asc)-window)+1):#0到總長-window,+1是因為不包括
+		tempsize=tempsize+1
+		if tempsize<minsize:
+			#print tempsize,minsize
+			continue
+		if check==0:
+			check=1			
+			r=0
+			j=0
+			#for j in range(i,i+window):
+			while j<(window):
+					r=(r*base+asc[i+j])%mod
+					j=j+1
+					#print r
+					pass
+		else:
+			r=((r-asc[i-1]*basewindow)*base+asc[i+window-1])%mod
+			#print (i+window-1),r,asc[i-1]*basewindow,asc[i+window-1]
+			#break			
+		if r % avsize ==0:			
+			hashv.append(r)
+			cutpoint.append(i+10)
+			sizeofchunk.append(tempsize)
+			#print tempsize
+			tempsize=0
+		if tempsize>=maxsize:
+			cutpoint.append(i+10)
+			sizeofchunk.append(tempsize)
+			#print tempsize
+			tempsize=0
+	#print tempsize
+	sizeofchunk.append(tempsize)	
+	#print "cuthash:",hashv
+	#print "cutpoint:",cutpoint
+	#print "chunksize:",sizeofchunk
+	#return cutpoint,sizeofchunk[len(sizeofchunk)-1]
+	#return sizeofchunk[len(sizeofchunk)-1]#最後一個	
+	return sizeofchunk,cutpoint
+
+def disktail(a):#算每個chunk的渣渣
+	tail=[]
+	num=0
+	for i in a:
+		num=num+i/clustersize	
+		m=i%clustersize
+		if m != 0:
+			tail.append(m)
+			num=num+1
+	
+	return tail,num
+
+
+def makefile(a,b):
+	#print len(b)
+	chunk=0
+	max=len(b)	
+	index=1	
+	f = open('%s-%d.txt' %(a.name.split(".")[0],index), 'w')	
+	for line in a:		
+		for c in line:
+			f.write(c)
+			chunk=chunk+1
+			if index < max:
+				if chunk == b[index-1]:
+					#print "cut",index
+					index=index+1
+					chunk=0
+					f = open('%s-%d.txt' %(a.name.split(".")[0],index), 'w')
+	print "cuts:",index
+
+
+for i in range(0,10):
+	with open('big%d.txt'%i) as infile:
+		cp=[]
+		(soc,cp)=rb(infile)
+		dtail=[]
+		(dtail,chunknum)=disktail(soc)
+		#print dtail
+
+		
+		
+			
+		infile.seek(0)
+		#print infile.name.split(".")[0]
+		#makefile(infile,soc)
+
+
+		tempsize=0
+		realsize=0
+
+		files=[]
+		combine=[]
+		for i in range(0,len(dtail)):
+			tempsize=tempsize+dtail[i]
+			files.append("chunk%d"%i)
+
+			if tempsize <= clustersize:
+				realsize=tempsize
+			else:
+				files.pop()
+				oup=str(realsize),files
+				files=[]
+				files.append("chunk%d"%i)
+				combine.append(oup)
+				tempsize=dtail[i]
+				realsize=dtail[i]
+			if i==(len(dtail)-1):
+				if realsize != 0:
+					oup=str(realsize),files
+					combine.append(oup)
+		#print combine
+		save=(len(dtail)-len(combine))*1.0/chunknum
+		print len(dtail),len(combine),save,chunknum
